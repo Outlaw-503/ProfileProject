a116f34cc8bf2f2392e6dca2c9e39bb0a69de989
Outlaw-503 <115690155+Outlaw-503@users.noreply.github.com>
2024-06-21 14:23:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 c=st.container()
 
 with c:
-  d=st.file_uploader(label="Upload Data",type="csv")
+  d=st.sidebar.file_uploader(label="Upload Data",type="csv")
   data=pd.read_csv(d)
   a=ProfileReport(data)
   st.write(a)
