bf149de173bb8d5f2b79685b08b319dd38038fcd
Outlaw-503 <115690155+Outlaw-503@users.noreply.github.com>
2024-06-21 14:58:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import pandas_profiling
 from ydata_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+from streamlit_ydata_profiling import st_profile_report
 import streamlit as st
 c=st.container()
 with st.sidebar.header ("Upload data"):
